b9bc125954a06da851b718eb7dcb485fc2f5fa5a
riyadmahmudkhan <mdriyadhossain356@gmail.com>
2022-08-21 03:39:29 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,48 @@
+function getPin() {
+    const pin = Math.round(Math.random() * 10000);
+    const pinString = pin + '';
+    if (pinString.length == 4) {
+        return pin;
+    }
+    else {
+        // console.log('got 3 digit and calling again', pin);
+        return getPin();
+    }
+}
+function generatePin() {
+    const pin = getPin();
+    document.getElementById('display-pin').value = pin;
+}
+
+document.getElementById('keypad').addEventListener('click', function (event) {
+    const number = event.target.innerText;
+
+    const calcInput = document.getElementById('typed-numbers');
+    if (isNaN(number)) {
+        if (number == 'C') {
+            calcInput.value = '';
+        }
+    }
+    else {
+        const previousNumber = calcInput.value;
+        const newNumber = previousNumber + number;
+        calcInput.value = newNumber;
+    }
+})
+
+function verifyPin() {
+    const pin = document.getElementById('display-pin').value;
+    const typedNumbers = document.getElementById('typed-numbers').value;
+    const succedMessage = document.getElementById('notify-succedd');
+
+    const failError = document.getElementById('notify-fail');
+
+    if (pin == typedNumbers) {
+        succedMessage.style.display = 'block';
+        failError.style.display = 'none';
+    }
+    else {
+        succedMessage.style.display = 'none';
+        failError.style.display = 'block';
+    }
+}
